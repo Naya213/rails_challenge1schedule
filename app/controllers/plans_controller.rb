f2902dd7ef402467b9376fbccf6f5f06a454e83b
Naya213 <20iia01aa21@gmail.com>
2023-09-20 10:12:09 +0900

    #{予定一覧ページと予定新規追加機能の作成}

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,31 @@
+class PlansController < ApplicationController
+  def index
+    @plans = Plan.all
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(params.require(:plan).permit(:title, :start_day, :finish_day, :all_day, :memo))
+     if @plan.save
+       flash[:notice] = "新規予定を追加しました"
+       redirect_to :plans
+     else
+       render "new"
+     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
